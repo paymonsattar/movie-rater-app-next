86329dd793544b986a0aa240e0a6100bee5bd357
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2023-09-04 23:57:30 +0100

    Reviews api unit tests

---
+++ backend/services/reviews/__tests__/reviewHandler.test.ts
@@ -0,0 +1,121 @@
+import { Request, Response } from 'express';
+import redis from 'redis-mock';
+import { addReview, getReviews, getReviewAverage } from '../src/handlers/reviewHandler'; // Replace with the actual module name
+import { RedisClient } from '../src/types'; // Replace with the actual module name
+
+jest.mock('redis', () => redis)
+
+// Mocking the Response object
+const mockResponse = () => {
+  const res: jest.Mocked<Response> = {
+    status: jest.fn().mockReturnThis() as any,
+    json: jest.fn().mockReturnThis() as any,
+  } as any;
+  return res;
+};
+
+// Mocking the Request object
+const mockRequest = (body: object, params: object): Request => {
+  return {
+    body,
+    params,
+  } as Request;
+};
+
+describe('Review Operations', () => {
+  let req: Request;
+  let res: jest.Mocked<Response>;
+  let mockRedisClient: ReturnType<typeof redis.createClient>;
+  let redisClient: RedisClient;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    res = mockResponse();
+    mockRedisClient = redis.createClient();
+
+    // It seems like there is a type mismatch between the RedisClient from
+    // 'redis-mock: v3.0.2' and the RedisClient from 'redis: v4.6.8'. For now asserting
+    // the type here, as tests are running correctly.
+    redisClient = mockRedisClient as unknown as RedisClient
+  });
+
+  // addReview test cases
+  describe('addReview', () => {
+    it('should successfully add a review', async () => {
+      req = mockRequest({ movieId: '1', review: '5' }, {});
+
+      await addReview(redisClient)(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.json).toHaveBeenCalledWith(expect.objectContaining({ message: 'Review added' }));
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      // Simulate an error using Promises
+      mockRedisClient.RPUSH = jest.fn().mockRejectedValue(new Error('Redis error'));
+
+      req = mockRequest({ movieId: '1', review: '5' }, {});
+    
+      await addReview(redisClient)(req, res);
+      
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Redis error' });
+    });
+  });
+  
+  // getReviews test cases
+  describe('getReviews', () => {
+    it('should successfully get reviews', async () => {
+      mockRedisClient.LRANGE = jest.fn().mockResolvedValue(['5', '4']);
+      req = mockRequest({}, { movieId: '1' });
+
+      await getReviews(redisClient)(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ reviews: ['5', '4'] });
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      mockRedisClient.LRANGE = jest.fn().mockRejectedValue(new Error('Redis error'));
+      req = mockRequest({}, { movieId: '1' });
+
+      await getReviews(redisClient)(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Redis error' });
+    });
+  });
+
+  // getReviewAverage test cases
+  describe('getReviewAverage', () => {
+    it('should successfully get review average', async () => {
+      mockRedisClient.LRANGE = jest.fn().mockResolvedValue(['5', '4']);
+      req = mockRequest({}, { movieId: '1' });
+
+      await getReviewAverage(redisClient)(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ average: 4.5 });
+    });
+
+    it('should return an average of 0 if no reviews are present', async () => {
+      mockRedisClient.LRANGE = jest.fn().mockResolvedValue([]);
+      req = mockRequest({}, { movieId: '1' });
+
+      await getReviewAverage(redisClient)(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({ average: 0 });
+    });
+
+    it('should return 500 if an error occurs', async () => {
+      mockRedisClient.LRANGE = jest.fn().mockRejectedValue(new Error('Redis error'));
+      req = mockRequest({}, { movieId: '1' });
+
+      await getReviewAverage(redisClient)(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Redis error' });
+    });
+  });
+});
--- backend/services/reviews/src/handlers/reviewHandler.ts
+++ backend/services/reviews/src/handlers/reviewHandler.ts
@@ -3,9 +3,8 @@ import { RedisClient } from '../types';
 
 export const addReview = (client: RedisClient) => async (req: Request, res: Response) => {
   const { movieId, review } = req.body;
-
   try {
-    const reply = await client.rPush(`reviews:${movieId}`, review.toString());
+    const reply = await client.RPUSH(`reviews:${movieId}`, review.toString());
 
     return res.status(201).json({ message: 'Review added', count: reply }); 
   } catch (error) {
@@ -17,7 +16,7 @@ export const getReviews = (client: RedisClient) => async (req: Request, res: Res
   const { movieId } = req.params;
 
   try {
-    const reply = await client.lRange(`reviews:${movieId}`, 0, -1);
+    const reply = await client.LRANGE(`reviews:${movieId}`, 0, -1);
 
     return res.status(200).json({ reviews: reply });
   } catch (error) {
@@ -29,7 +28,7 @@ export const getReviewAverage = (client: RedisClient) => async (req: Request, re
   const { movieId } = req.params;
 
   try {
-    const reply = await client.lRange(`reviews:${movieId}`, 0, -1);
+    const reply = await client.LRANGE(`reviews:${movieId}`, 0, -1);
     const reviews = reply.map(Number);
 
     const total = reviews.reduce((acc, val) => acc + val, 0);
