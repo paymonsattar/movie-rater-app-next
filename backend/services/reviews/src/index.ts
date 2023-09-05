8d7d6177db9fe19e30148153964a30c438746b94
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2023-09-05 12:13:14 +0100

    Update naming cont

--- backend/services/reviews/src/index.ts
+++ backend/services/reviews/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createClient } from 'redis';
-import { createMovieReviewRoutes } from './routes/reviews';
+import { createMovieReviewRoutes } from './routes/review';
 
 const client = createClient();
 
