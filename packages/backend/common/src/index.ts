03e10622af866bbf3f3256d6fdd29de51275615e
Paymon Sattarzadeh <47405092+paymonsattar@users.noreply.github.com>
2023-09-13 16:13:04 +0100

    Remove the need for tight couple

--- packages/backend/common/src/index.ts
+++ packages/backend/common/src/index.ts
@@ -1,37 +1,7 @@
-export { requestLogger } from './middleware/logger';
-
-export { paginate } from './utils/paginate';
-
-export { isValidEmail } from './utils/validate';
-
-export {
-  formatDate,
-  parseDate,
-  diffInDays,
-  addDays,
-  isValidDate,
-  toUnixTimestamp,
-} from './utils/date';
-
-export {
-  Movie,
-} from './types/movies';
-
-export {
-  Review,
-  AverageReview,
-} from './types/reviews';
-
-export {
-  IResponse,
-  sendHttpResponse,
-  OK_RESPONSE,
-  CREATED_RESPONSE,
-  NO_CONTENT_RESPONSE,
-  BAD_REQUEST_RESPONSE,
-  UNAUTHORIZED_RESPONSE,
-  FORBIDDEN_RESPONSE,
-  NOT_FOUND_RESPONSE,
-  INTERNAL_SERVER_ERROR_RESPONSE,
-  SERVICE_UNAVAILABLE_RESPONSE,
-} from './utils/response';
+export * from './middleware/logger';
+export * from './utils/paginate';
+export * from './utils/validate';
+export * from './utils/date';
+export * from './types/movies';
+export * from './types/reviews';
+export * from './utils/response';
